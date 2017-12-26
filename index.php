cb358423d9c5f9f089f0ef5c601395818dc5a95b
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-26 09:45:53 -0200

    Projeto Admin

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -16,6 +18,36 @@ $app->get('/', function() {
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+/*$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->pos('/admin/login',function(){
+
+	User::login($_POST["login"],$_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});*/
+
 
 $app->run();
 
