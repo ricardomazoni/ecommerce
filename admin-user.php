5859f15efccf456125b3fcb902fd7e6ab2c442fa
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-01-18 18:32:28 -0200

    Editar Produto

---
+++ admin-user.php
@@ -0,0 +1,65 @@
+<?php 
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+
+$app->get('/admin/users',function(){
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users",array(
+		"users"=>$users
+	));
+
+});
+
+$app->get('/admin/users/create',function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get('/admin/users/:iduser/delete',function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+
+$app->get('/admin/users/:iduser',function($iduser){
+
+	User::verifyLogin();
+
+	$user  = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update",array(
+			"user"=>$user->getValues()
+
+	));
+
+
+});
+
+
+ ?>
