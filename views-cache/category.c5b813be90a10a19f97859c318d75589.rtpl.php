2481ebd986153ee1bf2f8bfb750d54842ee38edd
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-01-23 12:00:51 -0200

    Paginacao

--- views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -23,7 +23,7 @@
                     </div>
                     <h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
                     <div class="product-carousel-price">
-                        <ins>R${function = "formatPrice($value.vlprice)" }</ins>
+                        <ins>R$<?php echo htmlspecialchars( $value1["vlprice"], ENT_COMPAT, 'UTF-8', FALSE ); ?></ins>
                     </div>  
                     
                     <div class="product-option-shop">
@@ -40,23 +40,13 @@
                 <div class="product-pagination text-center">
                     <nav>
                         <ul class="pagination">
-                        <li>
-                            <a href="#" aria-label="Previous">
-                            <span aria-hidden="true">«</span>
-                            </a>
-                        </li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li>
-                            <a href="#" aria-label="Next">
-                            <span aria-hidden="true">»</span>
-                            </a>
-                        </li>
+                        <?php $counter1=-1;  if( isset($pages) && ( is_array($pages) || $pages instanceof Traversable ) && sizeof($pages) ) foreach( $pages as $key1 => $value1 ){ $counter1++; ?>
+
+                        <li><a href="<?php echo htmlspecialchars( $value1["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
+                        <?php } ?>
+
                         </ul>
-                    </nav>                        
+                    </nav>
                 </div>
             </div>
         </div>
